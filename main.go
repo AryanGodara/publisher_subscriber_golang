3b0cb940a445c42cf73fb9faa792aba3a05701dc
aryan <aryangodara03@gmail.com>
2022-06-22 17:29:33 +0530

    basic http

--- main.go
+++ main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
-func BasicHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my website")
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()                   // parse arguments, you have to call this by yourself
+	fmt.Println("r.Form: ", r.Form) // print form info in server side
+	fmt.Println("path(r.URL.Path): ", r.URL.Path)
+	fmt.Println("scheme(r.URL.Scheme): ", r.URL.Scheme)
+	fmt.Println("(r.Form[\"url_long\"]", r.Form["url_long"])
+
+	for k, v := range r.Form {
+		fmt.Println("Key:", k)
+		fmt.Println("Val:", strings.Join(v, ""))
+	}
+
+	fmt.Fprintf(w, "Hello Aryan!") // sends data to client side
 }
 
 func main() {
-	http.HandleFunc("/", BasicHandler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", basicHandler)       // set router
+	err := http.ListenAndServe(":8080", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
